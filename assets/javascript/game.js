4e5d5e243a4e92fddd2a7f6b252215632663d9b7
Daniel Gonzalez <Daniel Gonzalez>
2017-02-07 00:52:29 -0500

    progress2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,76 @@
+window.onload = function () {
+alert("CHOOSE. YOUR. CHICKEN!");
+}
 
-
+/*array*/
 var chicken = ["fried", "barbecue", "grilled", "jerk"]
+
+//chicken type will be replaced by spaces equal to length of word
+var rawChicken = function () {
+var chosenChicken[Math.floor(Math.random() * chicken.length)];
+var hiddenChicken = chosenChicken.replace(/\s/g, "-");
+console.log(chosenChicken);
+buttons(); 
+
 var guessesLeft = 10;
 var losses = 0;
 var wins = 0;
 var previousGuesses = [];
+var letterGuess = String.fromCharCode()
+var chosenChicken = ;
+var hiddenChicken = ;
 
-var chosenChick = chicken[Math.floor(Math.random() * chicken.length)];
-
-var cooked = function () {
-word = chosenChick[Math.floor(Math.random() * chosenCategory.length)];
-word = word.replace(/\s/g, "-");
-console.log(word);
-buttons();
 }
+
+document.onkeyup = function() {
+letterGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+if (letterGuess === previousGuesses) {
+previousGuesses.push(letterGuess);
+console.log("Not a match!");
+
+if (letterGuess === letter in hiddenChicken) {
+
+letter in chosenChicken = hiddenChicken.replace(/\s/g, "-");
+    
+if(letterGuess === chicken[0-4]) {
+console.log("You got a chicken!");
+wins++;
+
+var rawChicken = function () {
+var chosenChicken[Math.floor(Math.random() * chicken.length)];
+var hiddenChicken = chosenChicken.replace(/\s/g, "-");
+console.log(chosenChicken);
+buttons(); 
+/*reset the number of guesses available*/
+function myFunction() {
+    delete previousGuesses[];
+
+}
+}
+else {
+guessesLeft--;
+        
+if(guessesLeft === 0) {
+console.log("you lost");
+losses++;
+
+//reset the number of guesses available
+var rawChicken = function () {
+var chosenChicken[Math.floor(Math.random() * chicken.length)];
+var hiddenChicken = chosenChicken.replace(/\s/g, "-");
+console.log(chosenChicken);
+buttons(); 
+
+function myFunction() {
+    delete previousGuesses[];
+}
+}
+
+}
+}
+else {
+console.log("You tried that already.");
+}
+}
+
