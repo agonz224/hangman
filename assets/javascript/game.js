378ce95eb3ab679454e3d3a32f12929d9fd6822d
Daniel Gonzalez <Daniel Gonzalez>
2017-02-08 23:56:57 -0500

    push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,12 +18,26 @@ console.log(chickPick)
 }
 console.log(dash)
 
+//game
+alert("guess that chicken");
+while(guessesLeft > 10){
+
+}
+
+  if (guessesLeft === 0) {
+    alert("game over");
+  }
+
 // keys pressed will be compared to letters in chickPick
 document.onkeyup = function(event) {
 var letterGuess = String.fromCharCode(event.keyCode).toLowerCase();
 for (var i = 0; i < chickPick.length; i++) 
 if (letterGuess === chickPick[i]) {
+  guessesLeft--;
 alert("match!");
+} else{
+  guessesLeft--;
+  alert("nope! " + " you have " + guessesLeft + " guesses left!")
 }
 }
 
